1a4f7768157fda58739ed340912212b83ab3e38e
Akeka_ <vladvaledol@gmail.com>
2025-05-20 12:09:40 +0300

    ws

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from server!")
+import uvicorn
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+players = {}
 
 
-if __name__ == "__main__":
-    main()
+@app.websocket('/ws')
+async def websocket(
+    websocket: WebSocket,
+):
+    await websocket.accept()
+    player = await websocket.receive_json()
+    print(player)
+
+    while True:
+        message = await websocket.receive_json()
+        print(message)
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', reload=True)
